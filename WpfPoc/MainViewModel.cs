f893693cebe1afce8ab8a2ffa4527faf59ff6f25
Humam <humam.abedrabbo@hotmail.com>
2023-03-07 16:24:02 +0200

    Add project files.

---
+++ WpfPoc/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfPoc
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        string firstName = "";
+        
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        string lastName = "";
+        
+        public string FullName => $"{FirstName} {LastName}";
+
+        [RelayCommand]
+        void Submit()
+        {
+            MessageBox.Show($"Hello {FullName}");
+        }
+
+        [RelayCommand]
+        Task DoIt()
+        {
+            Submit();
+            return Task.CompletedTask;
+        }
+    }
+}
