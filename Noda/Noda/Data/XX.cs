19942d089f2db5f3f39005cb3a5026f136a13a52
Humam <humam.abedrabbo@hotmail.com>
2023-09-04 14:22:13 +0300

    Add Noda test project

---
+++ Noda/Noda/Data/XX.cs
@@ -0,0 +1,33 @@
+﻿using NodaTime;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+using System.Text.RegularExpressions;
+
+namespace Noda.Data;
+
+public class XX
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Culture { get; set; } = "en-US";
+    public string TimeZone { get; set; } = "Asia/Damascus";
+    public string Calendar { get; set; } = "Gregorian";
+
+    public static IEnumerable<string> GetCultures() =>
+        CultureInfo.GetCultures(CultureTypes.AllCultures).Select(c => c.Name).OrderBy(x => x);
+    public static IEnumerable<string> GetTimeZones() =>
+        //DateTimeZoneProviders.Tzdb.Ids.OrderBy(x => x);
+        TimeZoneInfo.GetSystemTimeZones().Select(x => x.Id).OrderBy(x => x);
+    public static IEnumerable<string> GetCalendars(string culture) =>
+        //CalendarSystem.Ids.OrderBy(x => x);
+        (new CultureInfo(culture)).OptionalCalendars.Select(x => GetNormalizedName(x.GetType().Name))
+        .OrderBy(x => x);
+
+    public static string GetNormalizedName(string name)
+    {
+        Regex r = new Regex(
+    @"(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z])(?=[A-Z])|(?<=[A-Za-z])(?=[^A-Za-z])");
+
+        return r.Replace(name, " ");
+    }
+}
---
+++ Noda/Noda/Pages/Index.cshtml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Noda.Data;
+using NodaMoney;
+using NodaMoney.Extensions;
+using NodaTime;
+using System.Globalization;
+
+namespace Noda.Pages;
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        TestMoney();
+         //TestTime();
+        TestCalendar();
+        // TestFormatProviders();
+
+        ;
+    }
+
+    private void TestCalendar()
+    {
+        var company = new XX();
+        var cultures = XX.GetCultures();
+        var timezones = XX.GetTimeZones();
+        var calendars = XX.GetCalendars(company.Culture);
+        ;
+    }
+
+    private void TestTime()
+    {
+        Instant now = SystemClock.Instance.GetCurrentInstant();
+        ZonedDateTime nowInIsoUtc = now.InUtc();
+        ZonedDateTime nowInDamascus = now.InZone(DateTimeZoneProviders.Tzdb["Asia/Damascus"]);
+        ZonedDateTime nowHijri = now.InZone(DateTimeZoneProviders.Tzdb["Asia/Damascus"],
+            CalendarSystem.IslamicBcl);
+
+        
+
+        DateTime dt = DateTime.Now;
+
+        CultureInfo culture = new CultureInfo("ar-SY");
+        CultureInfo culture1 = new CultureInfo("ar-SA");
+        DateTimeZone timeZone = DateTimeZoneProviders.Tzdb["Asia/Damascus"];
+        DateTimeZone timeZone1 = DateTimeZoneProviders.Tzdb["Asia/Riyadh"];
+        ZonedDateTime nowZonedDateTime = new ZonedDateTime(Instant.FromDateTimeUtc(DateTime.Now.ToUniversalTime()), timeZone);
+        ZonedDateTime nowZonedDateTime1 = new ZonedDateTime(Instant.FromDateTimeUtc(DateTime.Now.ToUniversalTime()), timeZone1);
+        String nodaFormat = nowZonedDateTime.LocalDateTime.ToString(culture.DateTimeFormat.LongDatePattern, culture1);
+
+
+        var r = culture.DateTimeFormat;
+        r.Calendar = culture.OptionalCalendars[1];
+        ;
+
+        DateTime dtUtc = dt.ToUniversalTime();
+        LocalDateTime ldt = LocalDateTime.FromDateTime(dtUtc);
+
+        var newLdt = ldt.WithCalendar(CalendarSystem.IslamicBcl);
+        var ff = newLdt.ToString("D", culture1);
+
+        var london = DateTimeZoneProviders.Tzdb["Asia/Damascus"];
+        var before = london.AtStrictly(ldt);
+        ;
+    }
+
+    private static void TestMoney()
+    {
+        ;
+        var syp = NodaMoney.Currency.FromCode("SYP");
+        var pts = new NodaMoney.CurrencyBuilder("PTS", "virtual")
+        {
+            DecimalDigits = 0,
+            EnglishName = "Points",
+            Symbol = "pts",
+            ISONumber = "PTS"
+
+        }.Build();
+        
+        var all = Currency.GetAllCurrencies().ToList();
+       
+        Money m = new Money(1, syp);
+        Money m1 = new Money(1, pts);
+        ExchangeRate r = new ExchangeRate(syp, pts, 2,);
+        var y = m + r.Convert(m1);
+
+        var m2 = r.Convert(m);
+        var m3 = r.Convert(m1);
+        CultureInfo en = new CultureInfo("en-US");
+        CultureInfo sy = new CultureInfo("ar-SY");
+        Money k = new Money(560.2, Currency.FromCode("OMR"));
+        var v = k.ToString($"c{k.Currency.DecimalDigits}");
+        m = new Money(300, syp);
+        var s = m.ToString("I", sy.NumberFormat);
+    }
+}
